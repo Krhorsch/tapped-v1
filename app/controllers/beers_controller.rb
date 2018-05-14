0353125728be980d6fd5be95b9cad858e48696db
Krhorsch <krhorsch@yahoo.com>
2018-05-14 16:41:26 -0400

    new beer form

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,13 +1,24 @@
 class BeersController < ApplicationController
   def index
+    @beers = Beer.all
   end
 
   def show
   end
 
   def new
+    @beer = Beer.new
   end
 
   def create
+    @beer = Beer.new(beer_params)
+    @beer.save
+    redirect_to @beer
+  end
+
+  private
+
+  def beer_params
+    params.require(:beer).permit(:name, :brewery, :style)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Beer.create(name: "Lil Heaven", brewery: "Two Roads", style: "IPA")
+Beer.create(name: "Honeyspot Road", brewery: "Two Roads", style: "IPA")
+Beer.create(name: "#NOFILTER", brewery: "Thomas Hooker", style: "IPA")
+Beer.create(name: "Julius", brewery: "Treehouse", style: "IPA")
+Beer.create(name: "Head High", brewery: "Kane", style: "IPA")
+Beer.create(name: "Single Fin", brewery: "Kane", style: "Ale")
